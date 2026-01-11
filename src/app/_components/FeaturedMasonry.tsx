68eef7006e0be6e1e1040c8573c701b1e1d84c16
LucaasPhan <nhathuyphan.1903@gmail.com>
2026-01-12 01:04:59 +0700

    debug

--- src/app/_components/FeaturedMasonry.tsx
+++ src/app/_components/FeaturedMasonry.tsx
@@ -1,29 +1,9 @@
 "use client"
 import Image from "next/image"
 import { Photo } from "@/types/photo"
-import React from "react"
 import { Masonry } from "masonic"
 
 export default function FeaturedMasonry({ photos }: { photos: Photo[] }) {
-  const PhotoCard = React.memo(function PhotoCard({
-    photo,
-  }: {
-    photo: Photo
-  }) {
-    return (
-      <div className="overflow-hidden bg-gray-200 dark:bg-gray-800">
-        <Image
-          src={photo.imageUrl}
-          alt={photo.title}
-          width={photo.width}
-          height={photo.height}
-          className="w-full h-auto object-cover"
-          quality={100}
-        />
-      </div>
-    )
-  })
-
   return (
     <section className="mb-6">
       <Masonry
@@ -31,7 +11,16 @@ export default function FeaturedMasonry({ photos }: { photos: Photo[] }) {
         columnWidth={300}
         columnGutter={16}
         render={({ data }) => (
-          <PhotoCard photo={data}/>
+          <div className="overflow-hidden bg-gray-200 dark:bg-gray-800">
+            <Image
+              src={data.imageUrl}
+              alt={data.title}
+              width={data.width}
+              height={data.height}
+              className="w-full h-auto object-cover"
+              quality={100}
+            />
+          </div>
         )}
       />
     </section>
